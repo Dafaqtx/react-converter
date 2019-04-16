b3532e792fc2ea561a2c6a6f29028e44b982558f
Alexander <dafaqtx@gmail.com>
2019-04-16 10:44:38 +0300

    added class for getData

--- src/App.js
+++ src/App.js
@@ -2,7 +2,40 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import OpenWidgetButton from './components/OpenWidgetButton';
 
+import CurrencyLayer from './services/currencylayer';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.printAllCurrency();
+    this.printUSDCurrency();
+    this.printEURCurrency();
+    this.printRUBCurrency();
+  }
+  state = {
+    currency: {},
+  };
+
+  printAllCurrency() {
+    const currency = new CurrencyLayer();
+    currency.getAllCurrencies().then((data) => console.log(data));
+  }
+
+  printUSDCurrency() {
+    const currency = new CurrencyLayer();
+    currency.getUSDCurrency().then((data) => console.log(data));
+  }
+
+  printEURCurrency() {
+    const currency = new CurrencyLayer();
+    currency.getEURCurrency().then((data) => console.log(data));
+  }
+
+  printRUBCurrency() {
+    const currency = new CurrencyLayer();
+    currency.getRUBCurrency().then((data) => console.log(data));
+  }
+
   render() {
     return (
       <div id='app'>
---
+++ src/services/currencylayer.js
@@ -0,0 +1,37 @@
+class CurrencyLayer {
+  _apiBase = 'http://apilayer.net/api';
+  _apiKey = '17793dec281cda4014de31bb87f0c3e3';
+
+  async getResource(url) {
+    const res = await fetch(
+      `${this._apiBase}/live?access_key=${this._apiKey}&${url}`
+    );
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}`);
+    }
+    return await res.json();
+  }
+
+  async getAllCurrencies() {
+    const res = await this.getResource(`currencies=USD,EUR,RUB`);
+    return res.quotes;
+  }
+
+  async getUSDCurrency() {
+    const res = await this.getResource(`currencies=USD`);
+    return res.quotes;
+  }
+
+  async getEURCurrency() {
+    const res = await this.getResource(`currencies=EUR`);
+    return res.quotes;
+  }
+
+  async getRUBCurrency() {
+    const res = await this.getResource(`currencies=RUB`);
+    return res.quotes;
+  }
+}
+
+export default CurrencyLayer;
