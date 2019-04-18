674027487a67813f7b8c79485a90b743206b84ad
Alexander <dafaqtx@gmail.com>
2019-04-18 13:41:27 +0300

    refactor WidgetButton

--- src/components/widget-btn/widget-btn.js
+++ src/components/widget-btn/widget-btn.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
 
 class ControlButton extends Component {
+  state = {
+    currencies: [
+      {
+        id: 1,
+        name: 'RUR',
+        value: 0.016,
+        active: true,
+      },
+      {
+        id: 2,
+        name: 'USD',
+        value: 1,
+        active: false,
+      },
+      {
+        id: 3,
+        name: 'EUR',
+        value: 1.13,
+        active: false,
+      },
+    ],
+  };
   handleClick = () => {};
   render() {
-    const { curency } = this.props;
-
     return (
-      <button
-        onClick={() => this.handleClick()}
-        type='button'
-        className={`btn mr-2 ${curency.active ? 'btn-success' : 'btn-light'}`}
-      >
-        {curency.name}
-      </button>
+      <div className='mt-1 mb-4 controls'>
+        {this.state.currencies.map((curency) => (
+          <button
+            onClick={() => this.handleClick()}
+            key={curency.id}
+            type='button'
+            className={`btn mr-2 ${
+              curency.active ? 'btn-success' : 'btn-light'
+            }`}
+          >
+            {curency.name}
+          </button>
+        ))}
+      </div>
     );
   }
 }
--- src/components/widget/widget.js
+++ src/components/widget/widget.js
@@ -5,50 +5,19 @@ import WidgetDate from '../widget-date';
 import './widget.css';
 
 class Widget extends Component {
-  state = {
-    currencies: [
-      {
-        id: 1,
-        name: 'RUR',
-        value: 0.016,
-        active: true,
-      },
-      {
-        id: 2,
-        name: 'USD',
-        value: 1,
-        active: false,
-      },
-      {
-        id: 3,
-        name: 'EUR',
-        value: 1.13,
-        active: false,
-      },
-    ],
-  };
-
   render() {
     return (
       <div className='row justify-content-between'>
         <div className='col-6'>
           <span className='text-muted'>I have</span>
-          <div className='mt-1 mb-4 controls'>
-            {this.state.currencies.map((curency) => (
-              <ControlButton key={curency.id} curency={curency} />
-            ))}
-          </div>
+          <ControlButton />
           <div className='form-group'>
             <TextArea />
           </div>
         </div>
         <div className='col-6'>
           <span className='text-muted'>I need</span>
-          <div className='mt-1 mb-4 controls'>
-            {this.state.currencies.map((curency) => (
-              <ControlButton key={curency.id} curency={curency} />
-            ))}
-          </div>
+          <ControlButton />
           <div className='form-group'>
             <TextArea />
           </div>
