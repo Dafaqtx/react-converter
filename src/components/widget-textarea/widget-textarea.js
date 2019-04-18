c637223730957657f8742c50afd522169da5df30
Alexander <dafaqtx@gmail.com>
2019-04-18 16:34:56 +0300

    added regExp for TextArea component

--- src/components/widget-textarea/widget-textarea.js
+++ src/components/widget-textarea/widget-textarea.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
 class TextArea extends Component {
-  handleChange = () => {};
+  state = {
+    value: '',
+  };
+
+  handleChange = (event) => {
+    const value = event.target.value;
+    const regExp = /^[0-9.]+$/;
+
+    if (regExp.test(value)) {
+      this.setState({ value });
+    }
+  };
   render() {
     return (
       <div className='form-group'>
         <textarea
-          onChange={() => this.handleChange()}
+          value={this.state.value}
+          onChange={this.handleChange}
           className='form-textarea form-control no-resize'
           rows='3'
         />
