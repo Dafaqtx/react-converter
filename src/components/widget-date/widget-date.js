304990fcc3232210462fb3b0230d8b2ca42781f0
Alexander <dafaqtx@gmail.com>
2019-04-18 15:51:26 +0300

    added dynamic date into  component

--- src/components/widget-date/widget-date.js
+++ src/components/widget-date/widget-date.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 
 class WidgetDate extends Component {
+  state = {
+    date: null,
+  };
+
+  componentDidMount() {
+    const now = new Date().toString();
+    this.setState({ date: now });
+    this.updateDate();
+  }
+
+  updateDate = () => {
+    setInterval(() => {
+      const now = new Date().toString();
+      this.setState({ date: now });
+    }, 15000);
+  };
+
   render() {
     return (
       <div className='current-date'>
         <span className='current-date-label'>Данные за: </span>
-        {new Date().toString()}
+        {this.state.date}
       </div>
     );
   }
